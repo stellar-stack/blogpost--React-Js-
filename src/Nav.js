24ae5693c034ae6437265232ed5e59b99ddab224
elliot <elliotgould5721@gmail.com>
2024-08-29 10:22:37 +0100

    routing hooks and links

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,35 @@
-import React from 'react'
-
-const Nav = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+const Nav = ({ search, setSearch }) => {
   return (
-    <nav>
-        <h1>navigation</h1>
+    <nav className="Nav">
+      <form
+        action=""
+        className="searchForm"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <label htmlFor="search">Search Posts</label>
+        <input
+          type="text"
+          id="text"
+          placeholder="Search Posts"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/PostPage.js
+++ src/PostPage.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+const PostPage = ({ posts, handleDelete }) => {
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
 
-const PostPage = () => {
   return (
-    <main>
-      <h1>PostPage</h1>
-    </main>
-  )
-}
+    <main className="PostPage">
+      <article className="post">
+        {posts && (
+          <>
+            <h2>{post.title}</h2>
+            <p className="psotDate">{post.datetime}</p>
+            <p className="postBody">{post.body}</p>
 
-export default PostPage
+            <button onClick={() => handleDelete(post.id)}>Delete Post</button>
+          </>
+        )}
+
+        {!post && (
+          <>
+            <h2>Post not found</h2>
+
+            <p>
+              <Link to="/">Visit Our HomePage</Link>
+            </p>
+          </>
+        )}
+      </article>
+    </main>
+  );
+};
+
+export default PostPage;
