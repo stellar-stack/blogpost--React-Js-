b4708bd6aa7d478a798129b41e2b4f93acd640ec
elliot <elliotgould5721@gmail.com>
2024-08-29 08:14:45 +0100

    Router hooks and Links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-import './index.css';
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/post" element={<NewPost />} />
+
+        <Route path="/post/:id" element={<PostPage />} />
+
+        <Route path="about" element={About} />
+        <Route path="users/*" element={Missing} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router,Routes ,Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Routes>
       <Route path='/' Component={App} />  
+
+      </Routes>
     </Router>
   </React.StrictMode>
 );
